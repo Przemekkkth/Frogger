c5622439032589d3d3202729271b96bb85b27929
Przemek Badaruk <przemekkth@outlook.com>
2022-08-29 11:04:50 +0200

    refactor resources and add sfx

--- src/game.cpp
+++ src/game.cpp
@@ -1,15 +1,15 @@
 #include "game.h"
 
-const QString Game::PATH_TO_FROG_PIXMAP         = ":/res/frog.png";
-const QString Game::PATH_TO_DEAD_FROG_PIXMAP    = ":/res/frog_dead.png";
-const QString Game::PATH_TO_TILE_PIXMAP         = ":/res/tiles.png";
-const QString Game::PATH_TO_CAR_PIXMAP          = ":/res/car.png";
-const QString Game::PATH_TO_CAR_R_PIXMAP        = ":/res/cars_r.png";
-const QString Game::PATH_TO_LOG_PIXMAP          = ":/res/logs.png";
-const QString Game::PATH_TO_MELTING_FROG_PIXMAP = ":/res/melting_frog.png";
-const QString Game::PATH_TO_TURTLE_PIXMAP       = ":/res/turtle.png";
-const QString Game::PATH_TO_TURTLE_R_PIXMAP     = ":/res/turtle_r.png";
-const QString Game::PATH_TO_WIN_FROG_PIXMAP   = ":/res/frog_win.png";
+const QString Game::PATH_TO_FROG_PIXMAP         = ":/res/sprite/frog.png";
+const QString Game::PATH_TO_DEAD_FROG_PIXMAP    = ":/res/sprite/frog_dead.png";
+const QString Game::PATH_TO_TILE_PIXMAP         = ":/res/sprite/tiles.png";
+const QString Game::PATH_TO_CAR_PIXMAP          = ":/res/sprite/car.png";
+const QString Game::PATH_TO_CAR_R_PIXMAP        = ":/res/sprite/cars_r.png";
+const QString Game::PATH_TO_LOG_PIXMAP          = ":/res/sprite/logs.png";
+const QString Game::PATH_TO_MELTING_FROG_PIXMAP = ":/res/sprite/melting_frog.png";
+const QString Game::PATH_TO_TURTLE_PIXMAP       = ":/res/sprite/turtle.png";
+const QString Game::PATH_TO_TURTLE_R_PIXMAP     = ":/res/sprite/turtle_r.png";
+const QString Game::PATH_TO_WIN_FROG_PIXMAP     = ":/res/sprite/frog_win.png";
 
 QList<bool> Game::SWAMP_POS = {false, false, false, false};
 
